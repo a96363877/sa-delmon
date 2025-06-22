11c24150c0f698bf1375170d57f035966cd90e4b
a96363877 <a96363877@gmail.com>
2025-06-22 08:40:38 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -66,9 +66,7 @@ function App() {
         default:
           return (
             <div className="min-h-screen bg-gray-50 py-8">
-              <div className="container mx-auto px-4 max-w-2xl">
                 <PaymentForm />
-              </div>
             </div>
           )
       }
